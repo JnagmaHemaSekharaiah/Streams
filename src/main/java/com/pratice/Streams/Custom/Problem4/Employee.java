e417617e633123af72df1af92a1063a5098e983a
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-04-23 06:21:44 +0530

    Stream desending avarg salary of each employee

---
+++ src/main/java/com/pratice/Streams/Custom/Problem4/Employee.java
@@ -0,0 +1,55 @@
+package com.pratice.Streams.Custom.Problem4;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Employee
+{
+    private int id;
+    private String name;
+    private String department;
+    private int salary;
+
+    public Employee(int id, String name, String department, int salary)
+    {
+        this.id=id;
+        this.name=name;
+        this.department= department;
+        this.salary=salary;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public int getSalary() { return salary; }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = List.of
+                (
+                        new Employee(1, "A", "IT", 5000),
+                        new Employee(2, "B", "IT", 7000),
+                        new Employee(3, "C", "HR", 4000),
+                        new Employee(4, "D", "HR", 6000)
+                );
+
+
+        Map<String,Double> map= employeeList.stream().collect(Collectors.groupingBy(x->x.department,
+                 Collectors.averagingInt(x->x.salary)
+         ));
+
+        System.out.println(map);
+
+    }
+
+    }
